8e690303447253cd768a8333032b67a15e47e2cc
pmartynov <paul.martynov@gmail.com>
2017-06-24 23:08:57 +0300

    Remove old urls structure

--- steepshot_io/config/urls.py
+++ steepshot_io/config/urls.py
@@ -17,10 +17,7 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from steepshot_io.core import views
-
 urlpatterns = [
     url(settings.ADMIN_URL, admin.site.urls),
-    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'', include('core.urls', namespace="core")),
 ]
