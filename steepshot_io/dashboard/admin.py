628d1f419759340dfe2e41986adae5e91f0491ce
roman4hik <romanchikovevgeni@gmail.com>
2017-12-19 17:13:11 +0300

    fix model and added model in admin

--- steepshot_io/dashboard/admin.py
+++ steepshot_io/dashboard/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from steepshot_io.dashboard.models import DashboardUsers
 
 # Register your models here.
+
+
+class DashboardUsersAdmin(admin.ModelAdmin):
+    list_filter = ['username']
+
+admin.site.register(DashboardUsers, DashboardUsersAdmin)
+
--- steepshot_io/dashboard/models.py
+++ steepshot_io/dashboard/models.py
@@ -7,6 +7,12 @@ class DashboardUsers(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=255)
 
+    class Meta:
+        db_table = 'user_dashboard'
+
+    def __str__(self):
+        return self.username
+
     def save(self, *args, **kwargs):
         self.password = make_password(self.password)
         super(DashboardUsers, self).save(*args, **kwargs)
