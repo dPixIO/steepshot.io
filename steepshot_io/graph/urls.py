8fdb7d8c779830cf0240260fab0f8678f92b6e0e
roman4hik <romanchikovevgeni@gmail.com>
2018-02-12 15:20:52 +0300

    added graph ltv

--- steepshot_io/graph/urls.py
+++ steepshot_io/graph/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^posts/ratio/monthly$', PostsRatioMonthly.as_view(), name='ratio_monthly'),
 
     url(r'^timeouts/daily$', GetDailyTimeouts.as_view(), name='timeouts_daily'),
+    url(r'^stats/ltv$', GetLtvDaily.as_view(), name='ltv_daily'),
 
     url(r'^comments/count$', CommentsCount.as_view(), name='count_comments_weekly'),
 
