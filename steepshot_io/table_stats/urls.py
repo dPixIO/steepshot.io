be41dc319bbe0844f2ffbdb84191295298e59581
roman4hik <romanchikovevgeni@gmail.com>
2018-02-28 13:35:10 +0300

    added delegators table

--- steepshot_io/table_stats/urls.py
+++ steepshot_io/table_stats/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     url(r'^stats$', GetStatsTable.as_view(), name='stats_table'),
+    url(r'^delegators$', GetDelegatorsSteepshot.as_view(), name='delegators_table'),
 ]
